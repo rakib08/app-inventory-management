d5a7ce0b8a64385f864a84f3a616d941885f8c20
Rakib Uddin <mdrakib88022@gmail.com>
2026-01-29 00:13:35 +0600

    vercel

--- src/app.js
+++ src/app.js
@@ -23,7 +23,8 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
+
+module.exports = app;
--- src/prisma.js
+++ src/prisma.js
@@ -1,6 +1,9 @@
-const { PrismaClient } = require("@prisma/client");
+const { PrismaClient } = require('@prisma/client');
 
-// One PrismaClient for the whole app
-const prisma = new PrismaClient();
+const globalForPrisma = globalThis;
+
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
 module.exports = prisma;
